1a9cb24e5696494bcca20046b6b6b7a885bb48f2
adeel <adeelwaraich27@gmail.com>
2025-07-18 22:22:21 +0500

    class1

---
+++ class1.py
@@ -0,0 +1,6 @@
+
+#this will print on the terminal "hello world"
+# print statement is used to print something on the terminal
+# it is used to get output from the computer through the terminal
+
+print("hello world")
