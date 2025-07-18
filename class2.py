4fc49eefa62d399516d852b7d4d3d9eb462429cb
adeel <adeelwaraich27@gmail.com>
2025-07-18 22:22:53 +0500

    class2

---
+++ class2.py
@@ -0,0 +1,22 @@
+# comments
+
+# this is a print statement that is print hello world
+'''
+this is a 
+print statemtn 
+that is printing hello world
+in the string
+'''
+print("hello world")
+
+
+# Variables & basic types: int, float, str, bool
+
+# Variables
+
+#  variable is like a container , box that is used to store some information or data 
+
+name = "adeel" 
+age = 27
+height = 5.11
+is_student = True
