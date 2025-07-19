dcffe988992d075114d4fc47fc8e2168ebb26b91
adeel <adeelwaraich27@gmail.com>
2025-07-20 03:53:44 +0500

    class3

--- class2.py
+++ class2.py
@@ -1,22 +1,31 @@
 # comments
 
-# this is a print statement that is print hello world
+# this is a print statement that is prints hello world
+# print("hello world")
+
 '''
+single quote (')
+double quotes (")
 this is a 
 print statemtn 
 that is printing hello world
 in the string
 '''
-print("hello world")
 
 
 # Variables & basic types: int, float, str, bool
 
 # Variables
 
-#  variable is like a container , box that is used to store some information or data 
+#  variable is like a container , box that is used to store some
+# information or data 
 
 name = "adeel"   # string data type
-age = 27     # integet data type
+marks = "90"  # number in double quotes also are strings data type
+age = 27     # integer data type
 height = 5.11       # float data type
 is_student = True   # Boolean data type
+print(age * 2)  # output  54
+print(marks * 2)  # output 9090
+
+
---
+++ class3.py
@@ -0,0 +1,11 @@
+# `input()` function
+
+# print("Assalam o Alaikum! I'm Adeel")
+
+# name = input("what is your name: ")
+# age = input("what is your age: ")
+# print(f"Hi, Your name is {name} . Your age is {age}")
+
+x = "44"
+y = int(x)
+print(type(y)) 
