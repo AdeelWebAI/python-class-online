d39e0f244d9cf3eba13e86002c99ba4f65145bc6
adeel <adeelwaraich27@gmail.com>
2025-07-25 21:57:51 +0500

    class5

---
+++ class5.py
@@ -0,0 +1,15 @@
+# If-Else Statements
+# if, elif, else logic
+# age = int(input("Enter your age: "))
+
+# if age > 0 and age < 13:
+#     print("you are a kid")
+# elif age > 13 and age < 20:
+#     print("you are a teenager")
+# elif age > 20 and age < 40:
+#     print("you are a senior citizen")
+# elif age > 40 and age < 100:
+#     print("you are a old man")
+# else:
+#     print("your input is wrong")
+
