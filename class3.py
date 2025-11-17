659c419892954b6ef336b90c83e8f3bf3f17ddf7
adeel <adeelwaraich27@gmail.com>
2025-11-17 14:49:20 +0500

    string methods

---
+++ class3.py
@@ -0,0 +1,13 @@
+# string methods
+# First of all keep in mind that strings are 'immutable'. Means directly you can't change the strings. Every string method that you will apply that will make a new string using that method.
+
+a = "It is a string"
+# print(len(a)) # length of a string
+# print(a.upper()) # upper case
+# print(a.lower()) # lower case
+# print(a.rstrip("@!")) # strip the trailing characters from end
+# print(a.replace("Python","zero")) # replace characters or words
+# print(a.split(" ")) # it converts the string into a list
+# print(a.capitalize()) # this makes first character of string capitalize
+# print(a.center(20)) # it addes the number of spaces before the string
+# print(a.count("It")) # count the words or character in string
