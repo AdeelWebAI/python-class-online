586767e7cdfe66a5f2b15fc6567c0cc51bec724d
adeel <adeelwaraich27@gmail.com>
2025-11-19 02:07:12 +0500

    for loops

--- 6_class.py
+++ 6_class.py
@@ -1,6 +1,31 @@
-# for loops
+# loops in python 
 
-# y = [1,2,3,4,5]
-# print(y)
-for x in range(11):
-    print(x)
+# for loop at numbers
+"""
+for i in range(5):  # it will print the numbers 0 to 4
+    print(i)
+    
+for i in range(10,100,5): # it will print numbers 10 to 100 with step of 5
+    print(i)
+    
+# for loop and nested for loop at list 
+"""
+
+'''
+# for loop at strings
+
+me = "Adeel Waraich"
+
+for i in me:
+    print(i)
+    if (i == "e"):
+        print("e is special character")
+
+
+names = ["Adeel","Ahmad","Hamza","Abadullah"] 
+
+for name in names:
+    print(name)
+    for n in name:
+        print(n) 
+'''
