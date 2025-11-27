1655eb9190735c8fafc7b6d59f1bf4484413b2fc
adeel <adeelwaraich27@gmail.com>
2025-11-27 23:19:22 +0500

    cisco exam during class

---
+++ ciscoExam.py
@@ -0,0 +1,98 @@
+# Q.1
+# my_numbers = [10, 20, 30, 40, 50]
+
+# for i in range(4):
+#     my_numbers.insert(i, my_numbers[-1])
+
+# print(my_numbers)
+
+# Q.2
+
+# def compute_square(x):
+#     return x * x
+
+
+# def compute_quad(x):
+#     return compute_square(x) * compute_square(None)
+
+
+# print(compute_quad(4))
+
+# Q.3
+
+# print(1 // 2)
+# Q.4
+
+# def raise_power(base, exponent):
+#     return base ** exponent
+
+
+# print(raise_power(exponent=3, 2))
+# Q.5
+
+# z = 0
+# y = 10
+# x = y < z and z > y or y < z and z < y
+# print(x)
+
+# Q. 6
+
+# reserved keyword
+# continue = 3
+# true = 10 
+
+# Q.7
+
+# my_values = [3 * i for i in range(5)]
+
+
+# def modify_list(values):
+#     del values[values[2] // 3]
+#     return values
+
+
+# print(modify_list(my_values))
+
+# Q.8
+
+# x = 1
+# y = 2
+# x, y, z = x, x, y
+# z, y, z = x, y, z
+
+# print(x, y, z)
+
+# Q.9
+
+# a = 1
+# b = 0
+# a = a ^ b
+# b = a ^ b
+# a = a ^ b
+
+# print(a, b)
+
+# Q.10
+
+# def custom_function(value):
+#     if value % 3 == 0:
+#         return 1
+#     else:
+#         return 2
+# print(custom_function(custom_function(4)))
+
+# Q.11
+
+# inventory = ['apple', 'banana', 'cherry']
+# backup_inventory = inventory
+# del backup_inventory[:]
+
+# Q.12
+
+# first_input = input("Enter first number: ")
+# second_input = input("Enter second number: ")
+# print(second_input + first_input)
+
+# Q.13
+
+print("a", "b", "c", sep="sep")
