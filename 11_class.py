6eb77442b2360a3ccaad9fc3d99dc0ffc6497c7e
adeel <adeelwaraich27@gmail.com>
2025-12-26 00:29:11 +0500

    list methods

---
+++ 11_class.py
@@ -0,0 +1,13 @@
+lst = [1,2,3,4,3,2,4,5,6]
+lst2 = [7,8,9]
+"""
+lst.append(1) # append an item to the list
+lst.sort() # sort a list 
+lst.sort(reverse=True) # sort a list in reversed
+lst.reverse() # reverse a list
+print(lst.index(3)) #print the first occurance of the specific item
+print(lst.count(4)) # count the specific element in a list
+lst.extend(lst2) # add a new list in an existing list 
+lst.insert(2, 222) # adds some value in some specific index
+"""
+print(lst)
