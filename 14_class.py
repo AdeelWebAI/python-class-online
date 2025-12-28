f00917b961be61c5d6386e101a7e9d75d27d474e
adeel <adeelwaraich27@gmail.com>
2025-12-28 17:28:12 +0500

    f strings in python

---
+++ 14_class.py
@@ -0,0 +1,22 @@
+# f-strings in python 
+# f strings are used to to formate strings in python and it is added after the python version 3.6 onward
+# first we were formating strings like this method
+"""
+intro = "My name is {1} and I am from {0}"
+name = "Adeel"
+country = "Pakistan"
+
+print(intro.format(country, name))
+
+"""
+name = "Adeel"
+country = "Pakistan"
+income = 666.123445678
+print(f"My name is {name} and I am from {country} and my income is {income:.6f} dollars per year") # :.2f means after point only 2 digits will print 
+
+# if you want to print the {name} and {country} as it is in the string, you should use this method
+
+print(f"My name is {{name}} and I am from {{country}}") 
+
+calculation = f"{2*4}" # also we can perform calculations in f-string 
+print(type(calculation)) 
