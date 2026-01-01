e53ee5533eb4414b04ab22d155d4e854ba1ec8da
adeel <adeelwaraich27@gmail.com>
2026-01-01 14:51:49 +0500

    sets in python

---
+++ 16_class.py
@@ -0,0 +1,19 @@
+# sets in python 
+# sets are the unordered collection of data in python which don't allow the duplicate values in it.
+"""
+s = {1,2,3,4,5,4,3,2,4,5,6}
+print(s)  # output {1, 2, 3, 4, 5, 6}
+
+# to make an empty set we use 
+s = {} # empty dectionary
+s = set() # empty set 
+s = set([1,2,3,4,5,3,4,5,2,11])
+print(s) 
+
+# how to access the sets
+
+for value in s:
+    print(value) 
+"""
+
+#more examples to understand sets
