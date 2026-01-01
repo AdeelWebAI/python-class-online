e0ce3cc013edc622312645a3556cc3312be56930
adeel <adeelwaraich27@gmail.com>
2026-01-02 02:15:12 +0500

    half set methods

--- 16_class.py
+++ 16_class.py
@@ -14,6 +14,22 @@ print(s)
 
 for value in s:
     print(value) 
-"""
+# methods of sets
 
-#more examples to understand sets
+s1 = {1,2,3,4,5}
+s2 = {4,5,6,7,8}
+
+# union and intersectoin for sets
+
+s3 = s1.union(s2) # union of two sets
+s4 = s1.intersection(s2)
+print(s3) 
+print(s4) 
+"""
+capitals1 = {"Lahore","Karachi","Queta"} 
+capitals2 = {"Karachi", "Muzafarabad","Islamabad"}
+# capitals1.update(capitals2) # update the existing set and add other set in it
+
+capitals1.intersection_update(capitals2) # update with intersection of two sets
+
+print(capitals1) 
