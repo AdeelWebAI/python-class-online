1e52d698903e99573871082bc5652aa9cb8179e0
adeel <adeelwaraich27@gmail.com>
2026-01-13 00:03:55 +0500

    dic methods covered

--- 17_class.py
+++ 17_class.py
@@ -34,7 +34,42 @@ std1["is_eliible"] = True # adds another item to the dictionary
 list_std = list(std1) # converts dictionary's keys into the list 
 list_std = list(std1.values()) # converts dictionary's values into the list 
 list_std = list(std1.items()) # converts dictionary's values into the list 
-""" 
+
 numbers = {1:"adeel",3:"hamza",2:"ali"}
 new_std = sorted(numbers)
-print(new_std) 
+new_std = list(numbers)
+if 2 in new_std:
+    print("2 is available") # after converting into list we can find any number in list
+print(new_std)
+
+# looping techniques in dictionaries
+students = {"Adeel":"Lahore","Bahawal":"Karachi","Jamshaid":"Islamabad"}
+
+for key,value in students.items(): # to get full items (keys, values)
+    print(key,"is living in",value) 
+for key in students.keys(): # to get keys
+    print(key) 
+for value in students.values(): # to get values
+    print(value) 
+
+# also we have enumate function to for looping through the data types
+# for dictionaries
+for key,value in enumerate(students.items()):
+    print(key,value) 
+    
+# for lists
+
+student_list = ["Adeel","Bahawal","Jamshaid"] 
+
+for index in enumerate(student_list):
+    print(index) 
+
+# if we have to iterate more than one lists at the same time, we can use zip function
+
+questions = ["what is your name: ","What is your Age: ","Which is your city: "]
+answers = ["My name is Adeel",30,"Faisalabad"]
+
+for q,a in zip(questions, answers):
+    print(q,a) 
+    
+""" 
