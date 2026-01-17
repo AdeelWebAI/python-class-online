41c8138c277f337dae60e03c1ff4b70a26a61113
adeel <adeelwaraich27@gmail.com>
2026-01-17 14:36:07 +0500

    if else short hand statement

--- 5_class.py
+++ 5_class.py
@@ -1,31 +1,57 @@
 # If-Else Statements
 # if, elif, else logic
-# age = int(input("Enter your age: "))
+"""
+Docstring for 5_class
+age = int(input("Enter your age: "))
 
-# if age > 0 and age < 13:
-#     print("you are a kid")
-# elif age > 13 and age < 20:
-#     print("you are a teenager")
-# elif age > 20 and age < 40:
-#     print("you are a senior citizen")
-# elif age > 40 and age < 100:
-#     print("you are a old man")
-# else:
-#     print("your input is wrong")
-
-
-
-# nested if else
+if age > 0 and age < 13:
+    print("you are a kid")
+elif age > 13 and age < 20:
+    print("you are a teenager")
+elif age > 20 and age < 40:
+    print("you are a senior citizen")
+elif age > 40 and age < 100:
+    print("you are a old man")
+else:
+    print("your input is wrong")
 
 
-# a = 11
 
-# if a > 10:
-#     print("a is greater than 10")
-#     if a == 11:
-#         a += 1
-#         print(a)
-# elif a == 9:
-#     print("a is equal to 9")
-# else:
-#     print("a is smaller than 10")
+nested if else
+
+
+a = 11
+
+if a > 10:
+    print("a is greater than 10")
+    if a == 11:
+        a += 1
+        print(a)
+elif a == 9:
+    print("a is equal to 9")
+else:
+    print("a is smaller than 10")
+
+
+
+# short hand if else statement
+"""
+
+a = 33
+b = 33
+
+# print("A") if a > b else print("=") if a == b else print("B") 
+
+# a simple example 
+print("A and B are equal ") if a == b else "" 
+
+
+# Calculator using short hand if else statement
+
+num1 = int(input("Enter first Number: "))
+opr = str(input("Enter the operation: "))
+num2 = int(input("Enter second Number: ")) 
+
+print(num1 + num2) if opr == "+" else print(num1 - num2) if opr == "-" else print(num1 * num2) if opr == "*" else print(num1 / num2) if opr == "/" else print("Sorry! Please Enter a valid operator") 
+
+print("Sorry! An error occured, Maybe you Entered invalid operator") 
