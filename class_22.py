3780d78490697d9f241bfc7bc2e92f21296bbb84
adeel <adeelwaraich27@gmail.com>
2026-01-25 07:40:57 +0500

    os module start

---
+++ class_22.py
@@ -0,0 +1,16 @@
+# os module in python 
+ # os moudule is used to handle operating sysems operations in python
+ 
+import os
+ 
+print(os.getcwd()) # shows the current workng directory 
+
+# if (not os.path.exists("assets")):
+#     os.mkdir("assets")  # ignores condition if folder already exists
+
+# for i in range(1,10): 
+#     os.mkdir(f"assets/folder{i}") # make 10 folders inside assets folder in bulk 
+    
+
+# for i in range(1,10):
+#     os.rename(f"assets/folder{i}", f"assets/folder {i}" ) # rename the folders in bulk
