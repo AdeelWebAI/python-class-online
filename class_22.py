e19d7723215e97ad09f68d63298072a12943619c
adeel <adeelwaraich27@gmail.com>
2026-01-25 23:57:24 +0500

    os module medium stage

--- class_22.py
+++ class_22.py
@@ -2,15 +2,24 @@
  # os moudule is used to handle operating sysems operations in python
  
 import os
- 
+"""
 print(os.getcwd()) # shows the current workng directory 
 
-# if (not os.path.exists("assets")):
-#     os.mkdir("assets")  # ignores condition if folder already exists
+if (not os.path.exists("assets")):
+    os.mkdir("assets")  # ignores condition if folder already exists
 
-# for i in range(1,10): 
-#     os.mkdir(f"assets/folder{i}") # make 10 folders inside assets folder in bulk 
+for i in range(1,10): 
+    os.mkdir(f"assets/folder{i}") # make 10 folders inside assets folder in bulk 
     
 
-# for i in range(1,10):
-#     os.rename(f"assets/folder{i}", f"assets/folder {i}" ) # rename the folders in bulk
+for i in range(1,10):
+    os.rename(f"assets/folder{i}", f"assets/folder {i}" ) # rename the folders in bulk
+
+print(os.listdir()) # shows the current directories files in bulk
+for i in os.listdir():  # shows the same thing in sequence 
+    print(i) 
+os.remove("sample.py") # deletes files like sample.py (not folers)
+os.removedirs("assets") # deletes the empty folder 
+for i in range(1,10): 
+    os.removedirs(f"assets/folder {i}") # do the same in bulk
+""" 
