aa7e9d8a67b953fd2d015927e1c69cf352d3a15f
adeel <adeelwaraich27@gmail.com>
2026-02-11 01:24:26 +0500

    lambda function in python

---
+++ class_25.py
@@ -0,0 +1,14 @@
+# lambda function in python
+# an annomous function used to define a function without any name in one line.
+"""
+Docstring for class_25
+def double(x):
+    return x * 2
+double = lambda x : x * 2 
+#  with two arguments 
+def avg(x,y):
+    return (x +y) / 2
+double = lambda x,y : (x + y) / 2 
+
+print(double(4,8)) 
+"""
